b0a59c103256ac96a8cf2d7d1e0ee33c3f4438a4
abriel0402 <abchprod@gmail.com>
2023-01-22 01:19:37 -0500

    states for workout logging and calorie tracking

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import Base from "./components/Base";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import WorkoutLogging from "./components/WorkoutLogging";
+import CalorieTracking from "./components/CalorieTracking";
 
 function App() {
 
+  let WORKOUTS = []
+
+  const getData = (data) => {
+    WORKOUTS.push(data)
+  }
+  
   
 
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Base />} />
-        <Route path="/workoutlogging" element={<WorkoutLogging />} />
+        <Route path="/" element={<Base workouts={WORKOUTS}/>} />
+        <Route path="/workoutlogging" element={<WorkoutLogging onSubmit={getData}/>} />
+        <Route path="/calorietracking" element={<CalorieTracking />} />
       </Routes>
     </Router>
   );
